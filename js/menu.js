175e18767b6ae6edecf25d02ca96dce7de737594
sergiodev66 <santanasergio692@gmail.com>
2021-01-18 15:50:57 -0300

    segunda pagina web

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnmenu= document.getElementById("btn-menu");
+let nav= document.getElementById("nav");
+
+btnmenu.addEventListener('click',function(){
+    nav.classList.toggle('muestra');
+});
